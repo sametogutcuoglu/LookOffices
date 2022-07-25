8fb1a186c1514272bdd803483a7a139f1cc406ad
Samet <f.sametogutcuoglu@gmail.com>
2022-07-25 23:11:21 +0300

    Added Login UIViews

--- LookOffices/ViewController.swift
+++ LookOffices/ViewController.swift
@@ -11,9 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let mainURL = Bundle.main.infoDictionary?["BACKEND_URL"] as? String
         
-        print(mainURL)
     }
 
 
