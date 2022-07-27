d2ba80fa65998f3e2bb367876daf4397ddb0595d
Samet <f.sametogutcuoglu@gmail.com>
2022-07-27 13:34:26 +0300

    Fix register storyboard constraint

--- LookOffices/Register/Controller/RegisterViewController.swift
+++ LookOffices/Register/Controller/RegisterViewController.swift
@@ -15,10 +15,10 @@ class RegisterViewController : UIViewController {
     
     let triangle = CAShapeLayer()
     let logintext = UILabel()
+    var count = 0
     
     
     override func viewDidLoad() {
-        creatUITriangle()
         createLoginLabel()
     }
     
@@ -40,22 +40,26 @@ func creatUITriangle() {
     
     
     triangle.fillColor = UIColor.loginBackgroundColor.cgColor
-    triangle.path = createRoundedTriangle(width: view.frame.width + 50, height:view.frame.height / 4, radius: 10)
-    triangle.position = CGPoint(x: view.frame.midX, y: view.frame.midY - view.frame.height / 8)
+    
+    triangle.path = createRoundedTriangle(width: TriangleView.bounds.width, height: TriangleView.bounds.height, radius: 0)
+    
+    triangle.frame.origin.x = TriangleView.bounds.midX
+    
+    triangle.frame.origin.y = TriangleView.bounds.maxY
 
-    view.layer.addSublayer(triangle)
+    TriangleView.layer.addSublayer(triangle)
 }
 
     func createRoundedTriangle(width: CGFloat, height: CGFloat, radius: CGFloat) -> CGPath {
         // Draw the triangle path with its origin at the center.
-        let point1 = CGPoint(x: -width / 2, y: height / 2) // sol kenar
-        let point2 = CGPoint(x: 0, y: -height / 2)  // orta nokta
-        let point3 = CGPoint(x: width / 2, y: height / 2) // sağ kenar
+        let point1 = CGPoint(x: -width, y: height / 2) // sol kenar
+        let point2 = CGPoint(x: 0, y: -height)  // orta nokta
+        let point3 = CGPoint(x: width, y: height / 2) // sağ kenar
 
         let path = CGMutablePath()
         path.move(to: CGPoint(x: 0, y: height / 2))
         path.addArc(tangent1End: point1, tangent2End: point2, radius: radius)
-        path.addArc(tangent1End: point2, tangent2End: point3, radius: 40)
+        path.addArc(tangent1End: point2, tangent2End: point3, radius: radius)
         path.addArc(tangent1End: point3, tangent2End: point1, radius: radius)
         path.closeSubpath()
 
@@ -70,6 +74,17 @@ func creatUITriangle() {
         logintext.textAlignment = .left
         logintext.text = "Login"
         view.addSubview(logintext)
+    }
+    
+    override func viewDidLayoutSubviews()
+    {
+        
+        super.viewDidLayoutSubviews()
+        count+=1
+        
+        if count == 2 {
+            creatUITriangle()
+        }
     }
     
     @IBAction func clickSingUpButton(_ sender: Any) {
