6bd0e821b7d373a012c781ad0e2e68f65bb042b6
Samet <f.sametogutcuoglu@gmail.com>
2022-07-27 13:06:52 +0300

    Change CATextLayer to UILabel
    Login text change UILabel,
    Add new Logo

--- LookOffices/Register/Controller/RegisterViewController.swift
+++ LookOffices/Register/Controller/RegisterViewController.swift
@@ -11,13 +11,15 @@ import UIKit
 class RegisterViewController : UIViewController {
     
   
+    @IBOutlet weak var TriangleView: UIView!
     
     let triangle = CAShapeLayer()
-    let loginTextLayer = CATextLayer()
+    let logintext = UILabel()
     
     
     override func viewDidLoad() {
-        creatUITriangleAndLoginText()
+        creatUITriangle()
+        createLoginLabel()
     }
     
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
@@ -28,56 +30,49 @@ class RegisterViewController : UIViewController {
                 return
             }
             triangle.fillColor = UIColor.loginBackgroundColor.cgColor
-            loginTextLayer.foregroundColor = UIColor.loginTextBackgroundColor.cgColor
-            loginTextLayer.backgroundColor = UIColor.loginBackgroundColor.cgColor
             
             
         }
         
     }
 
-func creatUITriangleAndLoginText() {
+func creatUITriangle() {
     
     
     triangle.fillColor = UIColor.loginBackgroundColor.cgColor
-    triangle.path = createRoundedTriangle(width: view.frame.width + 50, height:view.frame.height / 4, radius: 0)
+    triangle.path = createRoundedTriangle(width: view.frame.width + 50, height:view.frame.height / 4, radius: 10)
     triangle.position = CGPoint(x: view.frame.midX, y: view.frame.midY - view.frame.height / 8)
+
     view.layer.addSublayer(triangle)
-    
+}
 
     func createRoundedTriangle(width: CGFloat, height: CGFloat, radius: CGFloat) -> CGPath {
         // Draw the triangle path with its origin at the center.
-        let point1 = CGPoint(x: -width / 2, y: height / 2)
-        let point2 = CGPoint(x: 0, y: -height / 2)
-        let point3 = CGPoint(x: width / 2, y: height / 2)
+        let point1 = CGPoint(x: -width / 2, y: height / 2) // sol kenar
+        let point2 = CGPoint(x: 0, y: -height / 2)  // orta nokta
+        let point3 = CGPoint(x: width / 2, y: height / 2) // sağ kenar
 
         let path = CGMutablePath()
         path.move(to: CGPoint(x: 0, y: height / 2))
-        path.addArc(tangent1End: point1, tangent2End: point2, radius: 10)
+        path.addArc(tangent1End: point1, tangent2End: point2, radius: radius)
         path.addArc(tangent1End: point2, tangent2End: point3, radius: 40)
-        path.addArc(tangent1End: point3, tangent2End: point1, radius: 10)
+        path.addArc(tangent1End: point3, tangent2End: point1, radius: radius)
         path.closeSubpath()
 
         return path
     }
     
-   
-
-    loginTextLayer.frame = CGRect(x: (view.frame.width - 100) / 2, y: view.frame.midY - view.frame.height / 9, width: 100, height: 40)
-    loginTextLayer.fontSize = 35
-    loginTextLayer.font = UIFont.boldSystemFont(ofSize: 50)
-    loginTextLayer.alignmentMode = .center
-    loginTextLayer.backgroundColor = UIColor.loginBackgroundColor.cgColor
-    loginTextLayer.string = "Login"
-    loginTextLayer.isWrapped = true
-    loginTextLayer.truncationMode = .end
-    loginTextLayer.foregroundColor = UIColor.loginTextBackgroundColor.cgColor
-
-    view.layer.addSublayer(loginTextLayer)
+    func createLoginLabel() {
+        
+        logintext.frame = CGRect(x: (view.frame.width - 100) / 2, y: view.frame.midY - view.frame.height / 9, width: 100, height: 40)
+        logintext.font = UIFont.boldSystemFont(ofSize: 40)
+        logintext.backgroundColor = .loginBackgroundColor
+        logintext.textAlignment = .left
+        logintext.text = "Login"
+        view.addSubview(logintext)
+    }
     
-    
-}
-    @IBAction func ClickSingUpButton(_ sender: Any) {
+    @IBAction func clickSingUpButton(_ sender: Any) {
         
         // TODO: Account created,Control E-mail and Password  min 8 character ,add scroll view
         
